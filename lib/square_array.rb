5257ef360f56990b79788f98f3f9b393085e9fa5
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-10 00:53:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,7 @@
-def array
-array = [1,2,3,4,5]
-
-square_array = []
-
 counter = 0
 array = [1,2,3,4,5]
  
 while array[counter] do
-  puts (array[counter])**2
+  puts array[counter]
   counter += 1
 end
-square_array
-end 
